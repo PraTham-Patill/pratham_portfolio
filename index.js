2e340c77a46f3323452b60fedb9780ae08305b71
Pratham Patil <129663114+PraTham-Patill@users.noreply.github.com>
2025-11-08 12:14:43 +0530

    d

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-console.log("Script running....")
+console.log("Script running...");
 
-document.querySelector('.cross').style.display='none';
+document.querySelector(".cross").style.display = "none";
 
-  document.querySelector('.hamburger').addEventListener("click",()=>{
-       document.querySelector('.sidebar').classList.toggle('sidebarGo');
-      
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".sidebar").classList.toggle("sidebarGo");
 
-       if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
-          document.querySelector('.ham').style.display='inline'
-     document.querySelector('.cross').style.display='none'
-       }
-     else{
-       document.querySelector('.ham').style.display='none'
-       setTimeout(()=>{
-            document.querySelector('.cross').style.display='inline'
-         },250)
-       }
-
-})
+  if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
+    document.querySelector(".ham").style.display = "inline";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector(".ham").style.display = "none";
+    setTimeout(() => {
+      document.querySelector(".cross").style.display = "inline";
+    }, 250);
+  }
+});
